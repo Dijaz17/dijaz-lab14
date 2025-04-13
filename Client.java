ffb2720ccb6459d4ebbbbd2e4341c38164cccadd
Dijaz17 <87551242+Dijaz17@users.noreply.github.com>
2025-04-12 22:32:59 -0400

    basic structure complete

--- Client.java
+++ Client.java
@@ -1,3 +1,28 @@
+import java.net.Socket;
+
 public class Client{
-    
+    private Socket socket;
+    public String host;
+    public int port;
+
+    public Client(String host, int port){
+        this.host = host;
+        this.port = port;
+    }   
+
+    public Socket getSocket(){
+        return this.socket;
+    }
+
+    public void handshake(){
+        
+    }
+
+    public String request(String input){
+        return "";
+    }
+
+    public void disconnect(){
+
+    }
 }
--- Server.java
+++ Server.java
@@ -1,3 +1,37 @@
+import java.net.Socket;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
 public class Server {
+    public int port;
+    private ArrayList<LocalDateTime> connectedTimes = new ArrayList<>();
     
+    public Server(int port){
+        this.port = port;
+    }
+
+    public void serve(int clients){
+
+    }
+
+    public ArrayList<LocalDateTime> getConnectedTimes(){
+        return connectedTimes;
+    }
+
+    public void disconnect(){
+
+    }
+
+    private class ClientHandler {
+        private Socket socket;
+        private LocalDateTime connectTime;
+
+        public ClientHandler(Socket socket) {
+            this.socket = socket;
+            this.connectTime = LocalDateTime.now();
+            connectedTimes.add(connectTime); 
+        }
+
+    }
+
 }
