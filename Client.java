46f5b731fcfcdfd83f8393a8b7ff05a9106b90df
Dijaz17 <87551242+Dijaz17@users.noreply.github.com>
2025-04-13 21:05:47 -0400

    completed Client and Server methods

--- Client.java
+++ Client.java
@@ -1,13 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
+import java.net.UnknownHostException;
 
 public class Client{
-    private Socket socket;
     public String host;
     public int port;
+    private Socket socket;
 
     public Client(String host, int port){
         this.host = host;
         this.port = port;
+        try {
+            socket = new Socket(host, port);
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }   
 
     public Socket getSocket(){
@@ -15,14 +27,36 @@ public class Client{
     }
 
     public void handshake(){
-        
+        try {
+            PrintWriter pw = new PrintWriter(socket.getOutputStream());
+            pw.println("12345");
+            pw.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public String request(String input){
-        return "";
+        String finalReq = "";
+        try {
+            PrintWriter pw = new PrintWriter(socket.getOutputStream());
+            pw.println(input);
+            pw.flush();
+            
+            BufferedReader br = new BufferedReader( new InputStreamReader(socket.getInputStream()) );
+            finalReq = br.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return finalReq;
+        
     }
 
     public void disconnect(){
-
+        try {
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- Server.java
+++ Server.java
@@ -1,3 +1,8 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
 import java.net.Socket;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -5,13 +10,24 @@ import java.util.ArrayList;
 public class Server {
     public int port;
     private ArrayList<LocalDateTime> connectedTimes = new ArrayList<>();
+    private ServerSocket sock;
     
     public Server(int port){
         this.port = port;
     }
 
     public void serve(int clients){
+        try {
+            sock = new ServerSocket(port);
 
+            for(int i = 0; i < clients; ++i){
+                ClientHandler clientHandler = new ClientHandler(sock.accept());
+                clientHandler.start();
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public ArrayList<LocalDateTime> getConnectedTimes(){
@@ -19,10 +35,14 @@ public class Server {
     }
 
     public void disconnect(){
-
+        try {
+            sock.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    private class ClientHandler {
+    private class ClientHandler extends Thread {
         private Socket socket;
         private LocalDateTime connectTime;
 
@@ -30,6 +50,11 @@ public class Server {
             this.socket = socket;
             this.connectTime = LocalDateTime.now();
             connectedTimes.add(connectTime); 
+        }
+
+        @Override
+        public void run() {
+            
         }
 
     }
